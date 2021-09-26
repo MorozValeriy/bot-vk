6433f15eea11e066fd85ef645b06b59718b86d70
kotek3d <cocacola3d1@gmail.com>
2021-09-26 19:32:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,9 +8,9 @@ const vk = new VK({
 })
 
 const bot = new HearManager()
-let count = 0;
 
 vk.updates.on('message_new', bot.middleware)
+randomCount = 4;
 
 bot.hear(/привет/i, msg => {
     msg.send('вас никто не слышит')
@@ -20,20 +20,41 @@ bot.hear(/пидарас/i, msg => {
     msg.send('что?')
 })
 
-count = 1;
-let messages = [];
-
-bot.onFallback(msg => {
-    if (msg.text !== undefined) {
-        messages.push(msg.text);
-    }
-
-    if (count === Math.floor(Math.random() * 4)) {
-        msg.send(random(messages.length - 1));
-    }
+bot.hear(/!command random/i, msg => {
+    randomCount = msg.text.toString().substring(16, 17);
+    msg.send('Шанс изменен');
 })
 
-function random(max){
+const messagesMap = new Map();
+
+bot.onFallback(msg => {
+    console.log(randomCount);
+    if (msg.text !== undefined) {
+        if (!messagesMap.has(msg.peerId)) {
+            let mess = [msg.text];
+            messagesMap.set(msg.peerId, mess);
+        } else {
+            messagesMap.get(msg.peerId).push(msg.text);
+        }
+    }
+
+        let messag = messagesMap.get(msg.peerId)
+        if (randomCount > 1) {
+            if (1 === Math.floor(Math.random() * randomCount)) {
+                if (messag.length > 1) {
+                    msg.send(random((messag.length - 1), messag));
+                }
+            }
+        } else if (randomCount == 1) {
+            console.log('true');
+            if (messag.length > 1) {
+                msg.send(random((messag.length - 1), messag));
+            }
+
+        }
+})
+
+function random(max, messages){
     let string = '';
     for (let i = 0; i < Math.floor(Math.random() * 5); i++) {
         string = string + ' ' + messages[Math.floor(Math.random() * max)];
@@ -50,3 +71,4 @@ app.listen(process.env.PORT || 5000 ,function(){
 
 console.log('Бот запущен');
 vk.updates.start().catch(console.error)
+
